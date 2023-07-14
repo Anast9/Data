d9d48e77c23c5f8ccbc65a1315bdf34250d5460f
Anast9 <102357076+Anast9@users.noreply.github.com>
2023-07-14 14:28:16 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+// logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+import Car from './Car/Car';
+class App extends Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    };
+
+    return (
+      <div style={divStyle}>
+        <h1> Hello world !!</h1>
+        <Car/>
+        <Car/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Car.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <div>
+        This is car component
+       <p> Number: <strong>{Math.round(Math.random() * 100) }</strong></p> 
+    </div>
+)
